2cf1f787e69f3e553fa8fa367cc9dd3103c36622
Bill Best <bill@wmbest2.(none)>
2010-10-13 00:33:05 -0400

    Initial Commit

---
+++ src/com/wmbest/etherguide/EtherGuide.java
@@ -0,0 +1,15 @@
+package com.wmbest.etherguide;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class EtherGuide extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
